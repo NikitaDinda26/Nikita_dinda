2e8e8e53e4fad4f8363a9784dc9bddd128054aad
NikitaDinda26 <amritadinda7@gmail.com>
2026-05-09 15:46:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("dash").style.display = "block";
+}
+
+function logout() {
+  document.getElementById("login").style.display = "block";
+  document.getElementById("dash").style.display = "none";
+}
